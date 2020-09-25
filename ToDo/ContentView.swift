4499182cb68636641f4546d5e10dc6fcfc7a5ced
zijievv <zijie_vv@icloud.com>
2020-09-25 13:27:57 +0800

    ToDo -- init

---
+++ ToDo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ToDo
+//
+//  Created by zijie vv on 25/09/2020.
+//  Copyright © 2020 zijie vv. All rights reserved.
+//
+//  ================================================================================================
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
