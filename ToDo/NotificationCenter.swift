bb64253ee686559d1f7488c26e7f7a2f780dfc1e
zijievv <zijie_vv@icloud.com>
2020-12-04 23:05:51 +0800

    Add NotificationCenter

---
+++ ToDo/NotificationCenter.swift
@@ -0,0 +1,44 @@
+//
+//  NotificationCenter.swift
+//  ToDo
+//
+//  Created by zijie vv on 04/12/2020.
+//  Copyright © 2020 zijie vv. All rights reserved.
+//
+//  ================================================================================================
+//
+
+import SwiftUI
+
+class NotificationCenter: NSObject, ObservableObject {
+    var dumbData: UNNotificationResponse?
+
+    override init() {
+        super.init()
+        UNUserNotificationCenter.current().delegate = self
+    }
+}
+
+extension NotificationCenter: UNUserNotificationCenterDelegate  {
+    func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        willPresent notification: UNNotification,
+        withCompletionHandler completionHandler:
+            @escaping (UNNotificationPresentationOptions) -> Void
+    ) {
+        //
+    }
+
+    func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        didReceive response: UNNotificationResponse,
+        withCompletionHandler completionHandler: @escaping () -> Void
+    ) {
+        dumbData = response
+    }
+
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                openSettingsFor notification: UNNotification?) {
+        //
+    }
+}
